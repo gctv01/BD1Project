91c9216fc39f5524015baa4ae07e02f9f708f44e
gctv01 <39320335+gctv01@users.noreply.github.com>
2018-12-04 23:15:14 -0400

    primeras vistas

---
+++ HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bd1.Models;
+
+namespace bd1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Homempleado()
+        {
+            return View();
+        }
+        public ActionResult Menuenvios()
+        {
+            return View();
+        }
+        public ActionResult Enviarpaquete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EnviarPaquete(ModeloPaquete model)
+        {
+
+            string name = model.Nombre;
+            string type = model.Tipo;
+            int weight= model.Peso;
+            string destination = model.Destino;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(ModeloRegistro model)
+        {
+
+            string name = model.Usuario;
+            int pass = model.Contrasena;
+            int pass1 = model.ContrasenaConfirmar;
+            string email = model.Correoelectronico;
+            int phone = model.Telefono;
+            string id = model.CedulaIdentidad;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ModeloLogIn model)
+        {
+
+            string name = model.Usuario;
+            int pass = model.Contrasena;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ modelo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace bd1.Models
+{
+    public class ModeloRegistro
+    {
+        [Required]
+        public string Cedula { get; set; }
+        public string Usuario { get; set; }
+        public string CedulaIdentidad { get; set; }
+        public string Correoelectronico { get; set; }
+        public int Telefono { get; set; }
+        public int Contrasena { get; set; }
+        public int ContrasenaConfirmar { get; set; }
+    }
+
+    public class ModeloLogIn
+    {
+        [Required]
+        public string Usuario { get; set; }
+        [Required]
+        public int Contrasena { get; set; }
+    }
+    public class ModeloPaquete
+    {
+        public string Tipo;
+        public string Nombre;
+        public int Peso;
+        public string Destino;
+    }
+}
