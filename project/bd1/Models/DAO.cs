2fb46885f10eb6b159d4a6f1e08701912ad6a7fe
Rafael <rouser4345@gmail.com>
2018-12-09 14:48:34 -0400

    tooodo

---
+++ project/bd1/Models/DAO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace bd1.Models
+{
+    public class Program
+    {
+        public const string connString = "Server=localhost;Port=5434;" +
+                    "User Id=postgres;Password=123;Database=Test;";
+
+        private static NpgsqlConnection connS = null;
+
+        public static NpgsqlConnection getInstanceDAO()
+        {
+            if (Program.connS != null)
+            {
+                return connS;
+            }
+            else
+            {
+                connS = new NpgsqlConnection(connString);
+                return connS;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+
+            NpgsqlConnection conn = Program.getInstanceDAO();
+            conn.Open();
+            string sql = "SELECT * FROM \"Persona\"";
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+
+            List<Persona> data = new List<Persona>();
+
+            while (dr.Read())
+            {
+                //Console.WriteLine("connection established");
+                data.Add(new Persona()
+                {
+                    nombre = dr[2].ToString(),
+                    apellido = dr[0].ToString(),
+                    ci = dr[1].ToString()
+                });
+            }
+            dr.Close();
+            conn.Close();
+
+            Console.WriteLine("Los datos obtenidos fueron");
+            foreach (Persona p in data)
+            {
+                Console.WriteLine("nombre: " + p.nombre + "\t apellido: " + p.apellido + "\t CI: " + p.ci);
+            }
+
+            //AHORA UN INSERT
+
+            /*NpgsqlConnection conn2 = Program.getInstanceDAO();
+            conn2.Open();
+            sql = "INSERT INTO \"Persona\" (\"Nombre\", \"CI\") VALUES ('Pep', 13579)";
+            NpgsqlCommand cmd2 = new NpgsqlCommand(sql, conn2);
+            cmd2.ExecuteNonQuery(); //ESTO SOLO EJECUTA EL COMANDO, NO HAY NADA QUE LEER
+            conn.Close();*/
+
+        }
+    }
+}
