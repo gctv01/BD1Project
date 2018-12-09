fc10e302f3a321fffba4c788b3ed3c0d2f223d3d
Rafael <rouser4345@gmail.com>
2018-12-09 13:50:39 -0400

    Toda la carpeta

---
+++ .bd1/bd1/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bd1.Models
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string ci { get; set; }
+    }
+}
