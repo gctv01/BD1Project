e0e1b5e92b81dccaed4240e64a5f640c26bb19c7
Rafael <rouser4345@gmail.com>
2018-12-10 22:27:00 -0400

    Cambios locales

--- project/bd1/Controllers/PruebaController.cs
+++ project/bd1/Controllers/PruebaController.cs
@@ -10,26 +10,26 @@ namespace bd1.Controllers
     public class PruebaController : Controller
     {
         // GET: Prueba
-        public ActionResult PruebaTabla()
-        {
-            DAO data = new DAO();
-            List<Persona> personas = data.obtenerPersonas();
+   //     public ActionResult PruebaTabla()
+   //     {
+   //         DAO data = new DAO();
+   //         List<Persona> personas = data.obtenerPersonas();
 
-            return View(personas);
-			//prueba
-        }
+   //         return View(personas);
+			////prueba
+   //     }
 
-        [HttpPost]
-        public ActionResult insertar(Persona model)
-        {
-            //string ci = model.ci;
-            //string nombre = model.nombre;
+   //     [HttpPost]
+   //     public ActionResult insertar(Persona model)
+   //     {
+   //         //string ci = model.ci;
+   //         //string nombre = model.nombre;
 
-            DAO data = new DAO();
+   //         DAO data = DAO.getInstanceDAO();
 
-            data.insertarPersona(model.ci, model.nombre);
+   //         data.insertarPersona(model.ci, model.nombre);
 
-            return View("~/Views/Home/Index.cshtml");
-        }
+   //         return View("~/Views/Home/Index.cshtml");
+   //     }
     }
 }
