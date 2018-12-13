f709dec68f100c3b11bedcdb9624687ee4ef832d
javier0997 <39354554+javier0997@users.noreply.github.com>
2018-12-12 22:56:55 -0400

    Add files via upload

---
+++ _Gastos_.sql
@@ -0,0 +1,10 @@
+insert into "Gastos" ("COD", "Nombre", "MontoTotal", "Pagado", "FK-SucursalG") values (1, 'Gerome', 2000, true, 1);
+insert into "Gastos" ("COD", "Nombre", "MontoTotal", "Pagado", "FK-SucursalG") values (2, 'Fransisco', 2100, true, 2);
+insert into "Gastos" ("COD", "Nombre", "MontoTotal", "Pagado", "FK-SucursalG") values (3, 'Ali', 2200, true, 3);
+insert into "Gastos" ("COD", "Nombre", "MontoTotal", "Pagado", "FK-SucursalG") values (4, 'Evonne', 2300, false, 4);
+insert into "Gastos" ("COD", "Nombre", "MontoTotal", "Pagado", "FK-SucursalG") values (5, 'Hilde', 2400, true, 5);
+insert into "Gastos" ("COD", "Nombre", "MontoTotal", "Pagado", "FK-SucursalG") values (1, 'Luz', 2000, true, 1);
+insert into "Gastos" ("COD", "Nombre", "MontoTotal", "Pagado", "FK-SucursalG") values (2, 'Agua', 2100, true, 2);
+insert into "Gastos" ("COD", "Nombre", "MontoTotal", "Pagado", "FK-SucursalG") values (3, 'Gas', 2200, true, 3);
+insert into "Gastos" ("COD", "Nombre", "MontoTotal", "Pagado", "FK-SucursalG") values (4, 'Salarios', 2300, false, 4);
+insert into "Gastos" ("COD", "Nombre", "MontoTotal", "Pagado", "FK-SucursalG") values (5, 'Comida', 2400, true, 5);
