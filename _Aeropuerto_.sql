ca4fdc1a99cb9669c75f39b9a5ee1e49c2223747
gctv01 <39320335+gctv01@users.noreply.github.com>
2018-12-14 00:00:45 -0400

    Add files via upload

---
+++ _Aeropuerto_.sql
@@ -0,0 +1,5 @@
+﻿insert into "Aeropuerto" ("COD", "CantTerminales", "CantPistas", "Capacidad", "FK-LugarAe", "FK-SucursalA") values (1, 43, 45, 84, 3, 20);
+insert into "Aeropuerto" ("COD", "CantTerminales", "CantPistas", "Capacidad", "FK-LugarAe", "FK-SucursalA") values (2, 89, 22, 93, 3, 63);
+insert into "Aeropuerto" ("COD", "CantTerminales", "CantPistas", "Capacidad", "FK-LugarAe", "FK-SucursalA") values (3, 5, 23, 14, 4, 92);
+insert into "Aeropuerto" ("COD", "CantTerminales", "CantPistas", "Capacidad", "FK-LugarAe", "FK-SucursalA") values (4, 31, 50, 26, 5, 31);
+insert into "Aeropuerto" ("COD", "CantTerminales", "CantPistas", "Capacidad", "FK-LugarAe", "FK-SucursalA") values (5, 74, 12, 64, 2, 61);
