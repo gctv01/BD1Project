960193422481cdd5d46c47c72a8957c43e0f4f17
javier0997 <39354554+javier0997@users.noreply.github.com>
2018-12-14 13:46:49 -0400

    Add files via upload

---
+++ _Ruta_.sql
@@ -0,0 +1,5 @@
+insert into "Ruta" ("COD", "Costo", "FK-Sucursal1", "Fk-Sucursal2") values (1, 1500, 1, 5);
+insert into "Ruta" ("COD", "Costo", "FK-Sucursal1", "Fk-Sucursal2") values (2, 1501, 2, 6);
+insert into "Ruta" ("COD", "Costo", "FK-Sucursal1", "Fk-Sucursal2") values (3, 1502, 3, 7);
+insert into "Ruta" ("COD", "Costo", "FK-Sucursal1", "Fk-Sucursal2") values (4, 1503, 4, 8);
+insert into "Ruta" ("COD", "Costo", "FK-Sucursal1", "Fk-Sucursal2") values (5, 1504, 5, 9);
