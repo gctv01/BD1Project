a3335353ed2e08f98d5816e64e27b0fad8e364c8
javier0997 <39354554+javier0997@users.noreply.github.com>
2018-12-13 21:37:05 -0400

    Add files via upload

---
+++ _Rol_.sql
@@ -0,0 +1,5 @@
+insert into "Rol" ("COD", "Nombre") values (1, 'Admin');
+insert into "Rol" ("COD", "Nombre") values (2, 'Empleado');
+insert into "Rol" ("COD", "Nombre") values (3, 'Cajero');
+insert into "Rol" ("COD", "Nombre") values (4, 'Cliente');
+insert into "Rol" ("COD", "Nombre") values (5, 'Supervisor');
