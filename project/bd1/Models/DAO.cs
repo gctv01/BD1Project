d8b2d99a52efc6c8b2830029b495d630a6a0fe44
gctv01 <39320335+gctv01@users.noreply.github.com>
2019-01-18 00:09:53 -0400

    Update DAO.cs

--- project/bd1/Models/DAO.cs
+++ project/bd1/Models/DAO.cs
@@ -10,7 +10,7 @@ namespace bd1.Models
     public class DAO
     {
         public const string connString = "Server=localhost;Port=5432;" +
-                    "User Id=postgres;Password=4592cf;Database=LogUCAB;";
+                    "User Id=postgres;Password=123;Database=LogUCAB;";
 
         private static NpgsqlConnection connS = null;
 
