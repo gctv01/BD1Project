4c978ea1a2ed6eb268fe7e9f2c509a418fe894c3
Giancarlo <giankitricerri@gmail.com>
2019-10-24 18:41:50 -0400

    Porque me dio la gana

--- Reporte 1 - Enunciado.sql
+++ Reporte 1 - Enunciado.sql
@@ -1,7 +1,9 @@
-﻿Select s."COD", s."Nombre", estado
+Select s."COD", s."Nombre", estado
 From "Sucursal" s, "Lugar" l, (Select "Nombre" as estado, "COD"
 		 From "Lugar"
 		 Where "Tipo"='Estado'
 		 Order by estado) x
 where s."FK-LugarS"=l."COD" and l."Fk-LugarL"=x."COD" 
 Order by estado, s."Nombre"
+
+DDDDDDDDDDDDDD
